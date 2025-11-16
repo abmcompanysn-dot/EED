2262d723625cea062d0d19babb86d2dde310b274
abmcompanysn-dot <abmcompnysn@gmail.com>
2025-11-16 08:58:04 +0000

    first commit

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+// Ce fichier centralise la configuration de l'application.
+
+// URL de déploiement de votre script Google Apps Script en tant qu'API web.
+// IMPORTANT : Chaque fois que vous redéployez votre script, vous devez mettre à jour cette URL.
+const API_URL = "https://script.google.com/macros/s/AKfycbxEBOjNhMexM_ZrulkKV7bLXP5rvqm9WNo5q-l36pYM6TWodTG0D5zUWZwyBlJkYpCrgg/exec";
