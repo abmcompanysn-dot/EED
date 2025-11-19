32dc63db9e44fd830488a968b2db418673c4e5e5
abmcompanysn-dot <abmcompnysn@gmail.com>
2025-11-19 22:59:05 +0000

    Message clair sur les modifications

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
 
 // URL de déploiement de votre script Google Apps Script en tant qu'API web.
 // IMPORTANT : Chaque fois que vous redéployez votre script, vous devez mettre à jour cette URL.
-const API_URL = "https://script.google.com/macros/s/AKfycbyZNJ939zcTUAgjjwqwWBXyio3KzLvZFnGcIf442sT_Bpy88y8ohlBiq1BarP1YtF2y7w/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxik1h_KkV5yqjMn15Pa3Wj5QtOBygp34A_Lfk29QAqMRyWwZ61t-3zX5vaQh5nV9PoUg/exec";
