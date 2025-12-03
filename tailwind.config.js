3294d4da6fd272632a459b466126034759558326
abmcompanysn-dot <abmcompnysn@gmail.com>
2025-12-03 23:31:36 +0000

    Message clair sur les modifications

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     "./js/**/*.js" // Scanne tous les fichiers .js dans un dossier js (si vous en avez un)
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-blue': '#005a9c',
+        'primary-red': '#e53935',
+        'card-bg': '#1e293b', // --card-bg
+        'border-color': '#334155', // --border-color
+        'text-secondary': '#94a3b8' // --text-secondary
+      },
+      fontFamily: {
+        'sans': ['Open Sans', 'sans-serif'],
+        'orbitron': ['Orbitron', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
